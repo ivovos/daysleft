23e14b803028ab9688dff0f6aa31af408cb5d8c8
Ivo Vos <vos.ivo@gmail.com>
2017-01-23 13:04:16 +1100

    Added countdown effect

--- app.js
+++ app.js
@@ -6,20 +6,59 @@ var app = new Vue({
 	el: '#main',
 	data: {
 		message: 'days left',
-		days: 0 + ' days left'
+		nrDaysLeft: 0,
+		endDate: moment([2017, 2, 2]),
+		startDate: moment([2016,11,30]),
+		totalNrOfDays: 0,
+		counterNr: 0
 	},
 	methods: {
 		daysLeft: function(){
-			endTime = moment([2017, 2, 3]);
-			difference = endTime.diff(moment(), 'days');
-			//console.log(difference)
-			this.days = difference;
+			this.nrDaysLeft = this.endDate.diff(moment(), 'days');
+		},
+		totalDays: function(){
+			this.totalNrOfDays = this.endDate.diff(this.startDate, 'days');
+		},
+		countDown: function(){
+			var count = this.totalNrOfDays;
+			var counter=setInterval(timer, 100); //1000 will  run it every 1 second\
+
+			function timer()
+			{
+			  count=count-1;			
+
+			  console.log(app.nrDaysLeft);
+
+			  if (count <= app.nrDaysLeft)
+			  {
+			     clearInterval(counter);
+			     console.log('ready!');
+			     return;
+			  }
+			 // console.log(count);
+			  app.counterNr = count;
+			//  console.log(app.counterNr);
+			}
 		}
 	},
 	created: function(){
 		this.daysLeft();
+		this.totalDays();
+		this.countDown();
 	}
 });
 
-//console.log(app.daysLeft());
 
+
+// Create a way to countdown the days
+
+// take the starting amount of days
+
+// display  the initial amoount of days
+
+// count down a day and display it
+
+// loop until date shown is days left
+
+
+// should it count down from the total nr or from the last number seen?
